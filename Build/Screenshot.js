c2226c4905d1994849ed2a78b0560a9106f92e09
KrailDerud <123017415+KrailDerud@users.noreply.github.com>
2023-10-04 16:23:05 +0600

    Add files via upload

---
+++ Build/Screenshot.js
@@ -0,0 +1,8 @@
+function downloadScreenshot(dataURL, filename) {
+    const link = document.createElement('a');
+    link.href = dataURL;
+    link.download = filename;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
